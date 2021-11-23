5118dddfc103c339b6eb0d204c0ebcc52fb7a66e
KanaNakajima <kana.nakajima1995@gmail.com>
2021-11-24 00:20:48 +0900

    各タイトルにアニメをつけた。projectsを2つに絞った

--- scrollToTop.js
+++ scrollToTop.js
@@ -18,13 +18,13 @@ $(function(){
 
 
   //SlideIn
-  $(function(){
-    $('a[href^="#"]').click(function() {
-      var speed = 400; // スクロールスピード(ミリ秒)
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-    });
-  });
+  // $(function(){
+  //   $('a[href^="#"]').click(function() {
+  //     var speed = 400; // スクロールスピード(ミリ秒)
+  //     var href= $(this).attr("href");
+  //     var target = $(href == "#" || href == "" ? 'html' : href);
+  //     var position = target.offset().top;
+  //     $('body,html').animate({scrollTop:position}, speed, 'swing');
+  //     return false;
+  //   });
+  // });
